2a41d52637c52d35da9141cc254d6f07c90f500e
SUFelix <102931141+SUFelix@users.noreply.github.com>
2022-04-14 09:30:38 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,437 @@
+﻿namespace OrderChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.brezenButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.allesweg = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BrezePreisLabel = new System.Windows.Forms.Label();
+            this.BaguettePreisLabel = new System.Windows.Forms.Label();
+            this.AmperlaiberlPreisLabel = new System.Windows.Forms.Label();
+            this.pdaPreisLabel = new System.Windows.Forms.Label();
+            this.abholDatumKalender = new System.Windows.Forms.MonthCalendar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.abholDatumTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // brezenButton
+            // 
+            this.brezenButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.brezenButton.Location = new System.Drawing.Point(499, 79);
+            this.brezenButton.Name = "brezenButton";
+            this.brezenButton.Size = new System.Drawing.Size(94, 29);
+            this.brezenButton.TabIndex = 0;
+            this.brezenButton.Text = "Hinzufügen";
+            this.brezenButton.UseVisualStyleBackColor = false;
+            this.brezenButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.Location = new System.Drawing.Point(499, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Hinzufügen";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button3.Location = new System.Drawing.Point(499, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Hinzufügen";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button4.Location = new System.Drawing.Point(499, 258);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Hinzufügen";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe Script", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(154, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 29);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Breze";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Segoe Script", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(154, 142);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(263, 29);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Baguette";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Segoe Script", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(154, 200);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(263, 29);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Amperlaiberl";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Segoe Script", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(154, 257);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(263, 29);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "Pain des Amis";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(54, 362);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(522, 159);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18"});
+            this.comboBox1.Location = new System.Drawing.Point(55, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(93, 28);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24"});
+            this.comboBox2.Location = new System.Drawing.Point(55, 141);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(93, 28);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBox3.Location = new System.Drawing.Point(54, 199);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(94, 28);
+            this.comboBox3.TabIndex = 11;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBox4.Location = new System.Drawing.Point(55, 256);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(93, 28);
+            this.comboBox4.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(423, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Stückpreis";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // allesweg
+            // 
+            this.allesweg.Location = new System.Drawing.Point(696, 411);
+            this.allesweg.Name = "allesweg";
+            this.allesweg.Size = new System.Drawing.Size(144, 29);
+            this.allesweg.TabIndex = 14;
+            this.allesweg.Text = "Alles löschen";
+            this.allesweg.UseVisualStyleBackColor = true;
+            this.allesweg.Click += new System.EventHandler(this.allesweg_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(783, 524);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 27);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox6.Location = new System.Drawing.Point(607, 524);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(145, 27);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(635, 501);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Bestellstatus";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(802, 501);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Gesamtpreis";
+            // 
+            // BrezePreisLabel
+            // 
+            this.BrezePreisLabel.AutoSize = true;
+            this.BrezePreisLabel.Location = new System.Drawing.Point(441, 82);
+            this.BrezePreisLabel.Name = "BrezePreisLabel";
+            this.BrezePreisLabel.Size = new System.Drawing.Size(50, 20);
+            this.BrezePreisLabel.TabIndex = 19;
+            this.BrezePreisLabel.Text = "label4";
+            this.BrezePreisLabel.Click += new System.EventHandler(this.BrezePreisLabel_Click);
+            // 
+            // BaguettePreisLabel
+            // 
+            this.BaguettePreisLabel.AutoSize = true;
+            this.BaguettePreisLabel.Location = new System.Drawing.Point(441, 149);
+            this.BaguettePreisLabel.Name = "BaguettePreisLabel";
+            this.BaguettePreisLabel.Size = new System.Drawing.Size(50, 20);
+            this.BaguettePreisLabel.TabIndex = 20;
+            this.BaguettePreisLabel.Text = "label4";
+            this.BaguettePreisLabel.Click += new System.EventHandler(this.BaguettePreisLabel_Click);
+            // 
+            // AmperlaiberlPreisLabel
+            // 
+            this.AmperlaiberlPreisLabel.AutoSize = true;
+            this.AmperlaiberlPreisLabel.Location = new System.Drawing.Point(441, 207);
+            this.AmperlaiberlPreisLabel.Name = "AmperlaiberlPreisLabel";
+            this.AmperlaiberlPreisLabel.Size = new System.Drawing.Size(50, 20);
+            this.AmperlaiberlPreisLabel.TabIndex = 21;
+            this.AmperlaiberlPreisLabel.Text = "label4";
+            // 
+            // pdaPreisLabel
+            // 
+            this.pdaPreisLabel.AutoSize = true;
+            this.pdaPreisLabel.Location = new System.Drawing.Point(441, 264);
+            this.pdaPreisLabel.Name = "pdaPreisLabel";
+            this.pdaPreisLabel.Size = new System.Drawing.Size(50, 20);
+            this.pdaPreisLabel.TabIndex = 22;
+            this.pdaPreisLabel.Text = "label4";
+            // 
+            // abholDatumKalender
+            // 
+            this.abholDatumKalender.Location = new System.Drawing.Point(672, 82);
+            this.abholDatumKalender.MinDate = new System.DateTime(2022, 4, 12, 0, 0, 0, 0);
+            this.abholDatumKalender.Name = "abholDatumKalender";
+            this.abholDatumKalender.TabIndex = 23;
+            this.abholDatumKalender.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.abholDatumKalender_DateChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(735, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Abholdatum";
+            // 
+            // abholDatumTextBox
+            // 
+            this.abholDatumTextBox.Location = new System.Drawing.Point(54, 527);
+            this.abholDatumTextBox.Name = "abholDatumTextBox";
+            this.abholDatumTextBox.Size = new System.Drawing.Size(522, 27);
+            this.abholDatumTextBox.TabIndex = 25;
+            this.abholDatumTextBox.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 607);
+            this.Controls.Add(this.abholDatumTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.abholDatumKalender);
+            this.Controls.Add(this.pdaPreisLabel);
+            this.Controls.Add(this.AmperlaiberlPreisLabel);
+            this.Controls.Add(this.BaguettePreisLabel);
+            this.Controls.Add(this.BrezePreisLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.allesweg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.brezenButton);
+            this.Name = "Form1";
+            this.Text = "OrderChecker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button brezenButton;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private RichTextBox richTextBox1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
+        private Label label1;
+        private Button allesweg;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label2;
+        private Label label3;
+        private Label BrezePreisLabel;
+        private Label BaguettePreisLabel;
+        private Label AmperlaiberlPreisLabel;
+        private Label pdaPreisLabel;
+        private MonthCalendar abholDatumKalender;
+        private Label label4;
+        private TextBox abholDatumTextBox;
+    }
+}
